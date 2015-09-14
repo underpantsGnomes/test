5bdb310bc3080823efd6d342393d119922d64bde
underpantsGnomes <zoobonusbox@gmail.com>
2015-09-14 10:37:05 +0300

    New model `library`

---
+++ models/library.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+
+class Library extends \yii\db\ActiveRecord
+{
+    public function relation(){
+        $query = Book::find()
+            ->addSelect(['book.*','author.*']);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+        $query->joinWith(['author' => function ($query) {
+            $query->from(['author']);
+        }]);
+        return $dataProvider;
+    }
+}
